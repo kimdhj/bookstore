b9211396af1007167a115037d0d3d4dbc030478e
Donghyun Kim <hyunhahaha13@gmail.com>
2022-07-24 20:59:43 +0900

    범준 test.java 수정

--- src/main/java/com/spring/BJDev/test.java
+++ src/main/java/com/spring/BJDev/test.java
@@ -5,5 +5,11 @@ public class test {
 		System.out.println("범준 죽어");
 	  	System.out.println("범준 잘생겼어");
 	  	System.out.println("죽어..?");
+	  	int bumjun = 10;
+	  	if(bumjun == 10) {
+	  		System.out.println("잘생겼어");
+	  	} else {
+	  		System.out.println("못생겼어");
+	  	}
 	}
 }
