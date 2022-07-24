58999d2047e3eaac0c450c774490e0c3ace4043d
qjawns0222 <ko02222@naver.com>
2022-07-24 21:05:22 +0900

    test update

--- src/main/java/com/spring/BJDev/test.java
+++ src/main/java/com/spring/BJDev/test.java
@@ -3,7 +3,8 @@ package com.spring.BJDev;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("범준 죽어");
-	  	System.out.println("범준 잘생겼어");
+	  	System.out.println("범준 잘ㄹㅇㄹ");
 	  	System.out.println("죽어..?");
+	  	int y=10;
 	}
 }
